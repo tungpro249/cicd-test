0806f658c22b5aed9da514c24a663fce37add4a3
thanh tung <tungt3922gmail.com>
2025-11-30 12:44:32 +0700

    push

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,7 @@
-import AvatarBase from "../../components/ui/avatar/Avartar";
-import InputBase from "../../components/ui/input/InputBase";
 
 const Home = () => {
   return (
-    <>
     <div>hello world</div>
-      {/* <InputBase />
-      <AvatarBase />; */}
-    </>
   );
 };
 
